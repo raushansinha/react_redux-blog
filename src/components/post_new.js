1fe16be4aa2a2f1905273d1c75b1cde6a96a87cf
Raushan Sinha <hr.raushan@gmail.com>
2018-07-03 00:15:58 -0400

    error text abd field color

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -30,15 +30,20 @@ class NewPost extends Component {
 
 
     renderField(field) {
+        const { meta : { touched, error}} = field;
+        const className=`form-group ${touched && error ? 'has-danger':''}`;
+
         return(
-            <div className="form-group"> 
+            <div className= {className}> 
                 <label>{field.label}</label>
                 <input 
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.touched ? field.meta.error : ''}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
